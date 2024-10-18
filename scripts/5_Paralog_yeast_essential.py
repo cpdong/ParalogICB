8de9c0a336e89d3ad6fd3f794b8b929123ef6da9
Chuanpeng <chuanpengdong@gmail.com>
2024-10-18 14:22:36 -0400

    Update 5_Paralog_yeast_essential.py

--- scripts/5_Paralog_yeast_essential.py
+++ scripts/5_Paralog_yeast_essential.py
@@ -10,8 +10,6 @@ yeastData=pd.read_csv('https://raw.githubusercontent.com/cpdong/imParalog/main/d
 yeastData = yeastData[yeastData["Source"] == 'OGEE v3']
 yeast_essential_gene = yeastData['hsapiens_homolog_ensembl_gene_id'].unique()
 
-
-# Running CodeML module function from PAML
 paralog_bioMart_file="Ensembl102_pcgene_paralog_bioMart_query_result.tsv"
 para_biomart = pd.read_csv(paralog_bioMart_file, header=0, sep="\t")
 para_biomart = para_biomart[["ensembl_gene_id","hsapiens_paralog_ensembl_gene"]].drop_duplicates(keep='first')
@@ -24,6 +22,4 @@ for index, row in para_biomart.iterrows():
         yeast_essentials.append(0)
         
 para_biomart['yeast_essentials'] = yeast_essentials;
-
 para_biomart.to_csv('Paralog_gene_yeast_essentials.tsv', index=False, sep='\t');
-#
